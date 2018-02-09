4d004bf7efd9cd174ba3a78ec6d1d9aeee0266cb
anovis <anovis@skbkontur.ru>
2018-02-09 12:02:28 +0500

    Подсветка таблиц для эврики

---
+++ evrika.js
@@ -0,0 +1,3 @@
+var node = document.createElement('style');
+node.innerHTML = "tbody tr:nth-child(2n+1){background-color:#f6f6f6;} tbody tr:hover{background-color:#ddd;}";
+document.body.appendChild(node);
